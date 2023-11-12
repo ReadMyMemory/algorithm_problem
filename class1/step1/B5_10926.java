9b5ffd225dd1ad51b0555adc8e911cbf9da6b847
ReadMyMemory <lame3925@gmail.com>
2023-11-12 16:56:34 +0900

    solved B5_10926

---
+++ class1/step1/B5_10926.java
@@ -0,0 +1,10 @@
+package class1.step1;
+
+import java.util.Scanner;
+
+public class B5_10926 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.next() + "??!");
+    }
+}
