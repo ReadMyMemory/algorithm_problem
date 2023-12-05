ce6cc3a38a0d0f0a59aff4eec58360d6717ccb1a
ReadMyMemory <lame3925@gmail.com>
2023-12-05 14:02:50 +0900

    solved B3_10810

---
+++ class1/step4/B3_10810.java
@@ -0,0 +1,31 @@
+    package class1.step4;
+
+    import java.io.*;
+    import java.util.StringTokenizer;
+
+    public class B3_10810 {
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken()); // 바구니 개수
+            int M = Integer.parseInt(st.nextToken()); // 공 index 개수
+
+            int[] arr = new int[N];
+            for(int i = 0; i < M; i++) {
+                StringTokenizer st2 = new StringTokenizer(br.readLine());
+                int start = Integer.parseInt(st2.nextToken());
+                int end = Integer.parseInt(st2.nextToken());
+                int index = Integer.parseInt(st2.nextToken());
+                 for (int j = start; j <= end; j++)
+                     arr[j-1] = index;
+            }
+            for(int printarr : arr)
+                bw.write(printarr+" ");
+            bw.flush();
+            br.close();
+            bw.close();
+        }
+    }
+
