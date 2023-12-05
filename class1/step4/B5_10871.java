9b3d37f84f989051e0a0971524039301d247d83f
ReadMyMemory <lame3925@gmail.com>
2023-12-05 10:02:36 +0900

    solved B5_10871

---
+++ class1/step4/B5_10871.java
@@ -0,0 +1,27 @@
+package class1.step4;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class B5_10871 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        StringTokenizer st2;
+
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
+
+        st2 = new StringTokenizer(br.readLine());
+        int I;
+        for(int i = 0; i < N; i++) {
+            if((I = Integer.parseInt(st2.nextToken())) < X)
+                bw.write(I + " ");
+        }
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
