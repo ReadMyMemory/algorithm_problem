0d82959c655dbdd4203970ea836463e4e0d2f2a5
ReadMyMemory <lame3925@gmail.com>
2023-12-06 18:39:31 +0900

    Solved B2_2577

---
+++ class1/B2_2577.java
@@ -0,0 +1,28 @@
+package class1;
+
+import java.io.*;
+
+public class B2_2577 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        int C = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[10]; // 0~9 자리 수 기억
+        long value = (long) A * B * C;
+        String str = String.valueOf(value);
+        for(int i = 0; i < str.length(); i++) {
+            int index = str.charAt(i) - '0';
+            arr[index]++;
+        }
+        for(int print : arr)
+            bw.write(print+"\n");
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
