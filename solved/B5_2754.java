3ba62f5547993224dafcdf1e075f0bc5d3727ee8
ReadMyMemory <lame3925@gmail.com>
2023-12-10 23:56:12 +0900

    Solved B5_2754

--- solved/B5_2754.java
+++ solved/B5_2754.java
@@ -7,6 +7,7 @@ public class B5_2754 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         String str  = br.readLine();
+        // 더 세련되게도 가능했겠지만, 시간이 없어서 걍 조건문 다 걸었다.
         if(str.equals("A+"))
             bw.write(4.3+"");
         if(str.equals("A0"))
@@ -24,7 +25,19 @@ public class B5_2754 {
         if(str.equals("C0"))
             bw.write(2.0+"");
         if(str.equals("C-"))
-            bw.write(2.0+"");
+            bw.write(1.7+"");
+        if(str.equals("D+"))
+            bw.write(1.3+"");
+        if(str.equals("D0"))
+            bw.write(1.0+"");
+        if(str.equals("D-"))
+            bw.write(0.7+"");
+        if(str.equals("F"))
+            bw.write(0.0+"");
+
+        bw.flush();
+        br.close();
+        bw.close();
     }
 
 
