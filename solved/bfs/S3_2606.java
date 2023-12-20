fc84f34085fcbac1afe5b8ff1db56e1f0b84c614
ReadMyMemory <lame3925@gmail.com>
2023-12-20 09:13:37 +0900

    S3_2606 아직 못품

---
+++ solved/bfs/S3_2606.java
@@ -0,0 +1,27 @@
+package solved.bfs;
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class S3_2606 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int computer = Integer.parseInt(br.readLine());
+        int pair = Integer.parseInt(br.readLine());
+        //서로가 연결되었음을 확인하는 것은 이차원 배열을 통해 기록한다.
+        /*
+        1 감염 시작...
+        1 - 3
+         */
+        boolean[][] arr = new boolean[computer][computer];
+        for(int i = 0; i < pair; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            arr[a][b] = true;
+            arr[b][a] = true;
+        }
+
+    }
+}
