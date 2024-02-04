a49cd3029551f965138954a124a5fe5bbc2fd292
Jaehyun Lee <122192096+ReadMyMemory@users.noreply.github.com>
2024-02-04 17:41:56 +0900

    B5_9086.java 업데이트

--- tempSubmit/B5_9086.java
+++ tempSubmit/B5_9086.java
@@ -17,24 +17,3 @@ public class B5_9086 {
     }
 }
 
-/* 이거 제출하면 됨.
-
-import java.io.*;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        int number = Integer.parseInt(br.readLine());
-        for(int i = 0; i < number; i++) {
-            String str = br.readLine();
-            bw.write(String.valueOf(str.charAt(0))+String.valueOf(str.charAt(str.length()-1))+"\n");
-        }
-        bw.flush();
-        br.close();
-        bw.close();
-    }
-}
-
- */
