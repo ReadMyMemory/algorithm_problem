bdb2c40576d0d347819b0deecb5a3332dd1e560a
ReadMyMemory <lame3925@gmail.com>
2024-02-11 23:12:17 +0900

    Solved B4_5543, Not Solved S4_1158

---
+++ solved/silver/S4_1158.java
@@ -0,0 +1,22 @@
+package solved.silver;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class S4_1158 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<Integer> print = new ArrayList<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        for(int i = 1; i <= N; i++) {
+            list.add(i);
+        }
+
+
+
+    }
+}
