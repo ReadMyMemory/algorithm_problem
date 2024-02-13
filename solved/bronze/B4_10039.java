a3d92ee6a2948fd8ae5706e76c1a068bad551888
ReadMyMemory <lame3925@gmail.com>
2024-02-13 20:21:16 +0900

    Solved B4_10039

---
+++ solved/bronze/B4_10039.java
@@ -0,0 +1,15 @@
+package solved.bronze;
+import java.io.*;
+public class B4_10039 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int sum = 0;
+        for(int i = 0; i < 5; i++) {
+            int temp = Integer.parseInt(br.readLine());
+            if(temp < 40)
+                temp = 40;
+            sum += temp;
+        }
+        System.out.println(sum/5);
+    }
+}
