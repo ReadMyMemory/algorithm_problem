91d013891430c184465254c49bd63e1728135a88
ReadMyMemory <lame3925@gmail.com>
2024-03-11 01:45:00 +0900

    Solved B4_10156

---
+++ solved/bronze/B4_10156.java
@@ -0,0 +1,18 @@
+package solved.bronze;
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class B4_10156 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        if(K * N >= M)
+            System.out.println(K * N - M);
+        else
+            System.out.println(0);
+    }
+}
