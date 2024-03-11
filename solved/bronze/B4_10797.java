c93479c0a58e94bc6b5a55bb99bcd18a69b3eeb7
ReadMyMemory <lame3925@gmail.com>
2024-03-12 00:24:44 +0900

    Solved B4_10797

---
+++ solved/bronze/B4_10797.java
@@ -0,0 +1,22 @@
+package solved.bronze;
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class B4_10797 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int number = Integer.parseInt(br.readLine());
+
+        st = new StringTokenizer(br.readLine());
+
+        int count = 0;
+        while(st.hasMoreTokens()) {
+            if(number == Integer.parseInt(st.nextToken()))
+                count++;
+        }
+
+        System.out.println(count);
+    }
+}
