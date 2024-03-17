d8f13a5433734fc44d56cddfe6bb28e8616045f9
ReadMyMemory <lame3925@gmail.com>
2024-03-18 02:15:32 +0900

    Solved B4_24263

---
+++ solved/bronze/B4_24263.java
@@ -0,0 +1,11 @@
+package solved.bronze;
+import java.io.*;
+
+public class B4_24263 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(Integer.parseInt(br.readLine()));
+        System.out.println(1);
+
+    }
+}
