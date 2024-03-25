f87890703079008ff55f3aa9068dbb6ff9f2f727
ReadMyMemory <lame3925@gmail.com>
2024-03-26 03:59:45 +0900

    Solved B4_5524

---
+++ solved/bronze/B4_5524.java
@@ -0,0 +1,15 @@
+package solved.bronze;
+import java.io.*;
+
+public class B4_5524 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int number = Integer.parseInt(br.readLine());
+        while(number --> 0)
+            bw.write(br.readLine().toLowerCase()+"\n");
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
