5e466207a13a73cc349666824fc9032542f64c9f
ReadMyMemory <lame3925@gmail.com>
2024-04-08 23:47:15 +0900

    Not Solved G2_1167

---
+++ solved_ac_step/class4/G2_1167.java
@@ -0,0 +1,13 @@
+package solved_ac_step.class4;
+import java.io.*;
+
+/*
+문제를 보면서 생각한 것.
+결국 트리의 지름은
+ */
+public class G2_1167 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    }
+}
