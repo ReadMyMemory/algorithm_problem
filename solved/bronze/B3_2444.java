07dc2a02fee376515428165d470816c7aa4da2a0
ReadMyMemory <lame3925@gmail.com>
2024-04-24 00:38:17 +0900

    Solved B3_2444

---
+++ solved/bronze/B3_2444.java
@@ -0,0 +1,35 @@
+package solved.bronze;
+import java.io.*;
+
+public class B3_2444 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        // 위쪽 삼각형 출력
+        for (int i = 1; i <= N; i++) {
+            // 공백 출력
+            for (int j = 1; j <= N - i; j++) {
+                System.out.print(" ");
+            }
+            // 별 출력
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // 아래쪽 삼각형 출력
+        for (int i = N - 1; i >= 1; i--) {
+            // 공백 출력
+            for (int j = 1; j <= N - i; j++) {
+                System.out.print(" ");
+            }
+            // 별 출력
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
