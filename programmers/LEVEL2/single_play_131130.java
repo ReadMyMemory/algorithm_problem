a105952101eb65c75d9a99a1148866b17637e789
ReadMyMemory <lame3925@gmail.com>
2024-07-19 21:51:59 +0900

    Not Solved 혼자 놀기의 달인 131130

---
+++ programmers/LEVEL2/single_play_131130.java
@@ -0,0 +1,16 @@
+package programmers.LEVEL2;
+
+/*
+핵심은 어떤 숫자로 시작해도 다시 시작 숫자로 돌아온다는 점
+8 -> 4 -> 7 -> 1 -> 8
+6 -> 5 -> 2 -> 6
+순환 구조이기 때문에 사실상 상자 그룹도 주어진 순간부터 정해져있다고 보면 된다.
+1, 4, 7, 8 / 2, 5, 6 / 3
+이런식이다 테스트 케이스의 경우에는
+ */
+public class single_play_131130 {
+        public int solution(int[] cards) {
+            int answer = 0;
+            return answer;
+        }
+}
