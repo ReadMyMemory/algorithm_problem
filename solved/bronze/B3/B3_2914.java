0e881bac3022a275a77b71e5e16bea793bbafd6a
ReadMyMemory <lame3925@gmail.com>
2024-07-31 01:45:26 +0900

    B3_2914

---
+++ solved/bronze/B3/B3_2914.java
@@ -0,0 +1,13 @@
+package solved.bronze.B3;
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class B3_2914 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int I = Integer.parseInt(st.nextToken());
+        System.out.println(A * (I - 1) + 1);
+    }
+}
