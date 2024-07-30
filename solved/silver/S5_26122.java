7198930b04dd79f8be9dfe95e53f927c38b31bb2
ReadMyMemory <lame3925@gmail.com>
2024-07-30 23:40:16 +0900

    Not Solved S5_26122

---
+++ solved/silver/S5_26122.java
@@ -0,0 +1,25 @@
+package solved.silver;
+import java.io.*;
+import java.util.Stack;
+
+/*
+스택쓰면 될 거 같다.
+
+ */
+public class S5_26122 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Integer> stack = new Stack<>();
+
+        int K = Integer.parseInt(br.readLine());
+        String s = br.readLine();
+        int max = 0;
+
+        //문자열 스택에 입력
+        for(int i = 0; i < K; i++) {
+            if(s.charAt(i) == 'N')
+
+        }
+
+    }
+}
