b7620072117687e3e75ecc4e796f66945d725d54
ReadMyMemory <lame3925@gmail.com>
2024-08-13 00:04:17 +0900

    Solved B5_5337

---
+++ solved/bronze/B5/B5_5337.java
@@ -0,0 +1,9 @@
+package solved.bronze.B5;
+
+public class B5_5337 {
+        public static void main(String[] args) {
+            System.out.println(".  .   .");
+            System.out.println("|  | _ | _. _ ._ _  _");
+            System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+        }
+}
