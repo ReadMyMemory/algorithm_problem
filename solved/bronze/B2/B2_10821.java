022f4b4d623ee0cb5998dfa2cb61773d4de782e8
ReadMyMemory <lame3925@gmail.com>
2024-10-28 00:21:14 +0900

    Solved B2_10821

---
+++ solved/bronze/B2/B2_10821.java
@@ -0,0 +1,12 @@
+package solved.bronze.B2;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class B2_10821 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] list = (br.readLine().split(","));
+        System.out.println(list.length);
+    }
+}
