4c1f4ae0db8126185ad59339f8b40244268eec00
ReadMyMemory <lame3925@gmail.com>
2024-11-26 01:11:36 +0900

    Solved B4_31403, Not Solved G5_1647

---
+++ solved_ac_step/class5/G5_1647.java
@@ -0,0 +1,19 @@
+package solved_ac_step.class5;
+import java.io.*;
+
+/*
+최소 신장 트리
+가작 적은 비용으로, 한번만 거쳐 모든 정점을 연결하는 트리
+
+해당 트리를 완성하기 위해 트림 알고리즘 또는 크루스칼 알고리즘 사용 가능
+간선이 많으면 전자를, 적으면 크루스칼 사용 추천
+
+크루스칼은 자기 자신을 root로 생각하고
+
+ */
+public class G5_1647 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    }
+}
