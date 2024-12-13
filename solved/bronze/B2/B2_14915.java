aabbaa1bab1967753dc714aa394337380ad641ec
ReadMyMemory <lame3925@gmail.com>
2024-12-14 03:17:43 +0900

    Solved B2_14915

---
+++ solved/bronze/B2/B2_14915.java
@@ -0,0 +1,17 @@
+package solved.bronze.B2;
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class B2_14915 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        bw.write(Integer.toString(m,n).toUpperCase());
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
