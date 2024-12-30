7e582a3f0757fa237a53ab6a0b241aa90d9faa14
ReadMyMemory <lame3925@gmail.com>
2024-12-31 01:12:13 +0900

    Solved B4_11283

---
+++ solved/bronze/B4/B4_11283.java
@@ -0,0 +1,13 @@
+package solved.bronze.B4;
+import java.io.*;
+/*
+유니코드 값 대조로 풀 수 있을 듯 하다.
+참고로 가는 44032더라.
+ */
+
+public class B4_11283 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine().charAt(0) - '가' + 1);
+    }
+}
