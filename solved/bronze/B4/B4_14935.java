11cd28a1bd105feea522fe9e40b3eadc272b9fc6
ReadMyMemory <lame3925@gmail.com>
2025-01-01 05:37:34 +0900

    Solved B4_14935

---
+++ solved/bronze/B4/B4_14935.java
@@ -0,0 +1,16 @@
+package solved.bronze.B4;
+import java.io.*;
+
+/*
+한 자리 수는 FA가 되고,
+그보다 자릿수가 크면 언젠가 한 자리 수가 될 것이기 때문에
+모두가 FA 수다.
+ */
+
+public class B4_14935 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        br.readLine();
+        System.out.println("FA");
+    }
+}
