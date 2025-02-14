869ea6d6e476fbb09a3355ef096be19356e91b9b
ReadMyMemory <lame3925@gmail.com>
2025-02-15 01:00:56 +0900

    Solved B2_10870

---
+++ solved/bronze/B2/B2_10870.java
@@ -0,0 +1,23 @@
+package solved.bronze.B2;
+import java.io.*;
+
+public class B2_10870 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(fibonacci(n));
+    }
+
+    private static int fibonacci(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        int a = 0, b = 1, result = 0;
+        for (int i = 2; i <= n; i++) {
+            result = a + b;
+            a = b;
+            b = result;
+        }
+        return result;
+    }
+}
