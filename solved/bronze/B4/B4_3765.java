e56f448ebda61057dc95e0846cd17c11710297fd
ReadMyMemory <lame3925@gmail.com>
2025-02-18 01:38:44 +0900

    Solved B4_3765

---
+++ solved/bronze/B4/B4_3765.java
@@ -0,0 +1,14 @@
+package solved.bronze.B4;
+
+import java.io.*;
+
+public class B4_3765 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s;
+
+        while ((s = br.readLine()) != null)
+            System.out.println(s);
+
+    }
+}
