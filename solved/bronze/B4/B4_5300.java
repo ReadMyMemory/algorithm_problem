1b2e353071af6f59c7dd5c26403534e0f562f07a
ReadMyMemory <lame3925@gmail.com>
2025-03-26 00:47:57 +0900

    Solved B4_5300

---
+++ solved/bronze/B4/B4_5300.java
@@ -0,0 +1,17 @@
+package solved.bronze.B4;
+import java.io.*;
+public class B4_5300 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= N; i++) {
+            sb.append(i).append(" ");
+            if (i % 6 == 0 || i == N) {
+                sb.append("Go! ");
+            }
+        }
+        System.out.println(sb.toString().trim());
+    }
+}
