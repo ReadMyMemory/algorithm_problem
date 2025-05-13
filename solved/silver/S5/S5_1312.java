7a5d39e99260c92286aab2bd6163c15acbb3fd9c
ReadMyMemory <lame3925@gmail.com>
2025-05-14 04:32:58 +0900

    Solved S5_1312

--- solved/silver/S5/S5_1312.java
+++ solved/silver/S5/S5_1312.java
@@ -1,24 +1,24 @@
-package solved.silver.S5;
-
-import java.io.*;
-import java.util.StringTokenizer;
-
-public class S5_1312 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int A = Integer.parseInt(st.nextToken());  // 분자
-        int B = Integer.parseInt(st.nextToken());  // 분모
-        int N = Integer.parseInt(st.nextToken());  // 원하는 소수 자릿수
-
-        int remainder = A % B;
-        int digit = 0;
-        // 1번째 소수 자리부터 N번째까지 반복
-        for (int i = 1; i <= N; i++) {
-            remainder *= 10;
-            digit = remainder / B;
-            remainder %= B;
+    package solved.silver.S5;
+    
+    import java.io.*;
+    import java.util.StringTokenizer;
+    
+    public class S5_1312 {
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());  // 분자
+            int B = Integer.parseInt(st.nextToken());  // 분모
+            int N = Integer.parseInt(st.nextToken());  // 원하는 소수 자릿수
+    
+            int remainder = A % B;
+            int digit = 0;
+            // 1번째 소수 자리부터 N번째까지 반복
+            for (int i = 1; i <= N; i++) {
+                remainder *= 10;
+                digit = remainder / B;
+                remainder %= B;
+            }
+            System.out.println(digit);
         }
-        System.out.println(digit);
     }
-}
