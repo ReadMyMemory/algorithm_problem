594ceb9f630b62995d133006dc0fd386a3c4dcc8
ReadMyMemory <lame3925@gmail.com>
2025-06-27 02:27:05 +0900

    Solved B4_33701

---
+++ solved/bronze/B4/B4_33701.java
@@ -0,0 +1,9 @@
+package solved.bronze.B4;
+import java.io.*;
+
+public class B4_33701 {
+    public static void main(String[] args) throws IOException {
+        String buildingName = "nkuugwan";
+        System.out.println(buildingName);
+    }
+}
