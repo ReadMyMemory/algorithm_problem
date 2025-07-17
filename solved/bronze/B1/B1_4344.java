59bb71484a81afd9da98cfa002f5908119358752
이재현 <122192096+ReadMyMemory@users.noreply.github.com>
2025-07-18 01:33:44 +0900

    Solved B1_4344

---
+++ solved/bronze/B1/B1_4344.java
@@ -0,0 +1,33 @@
+package solved.bronze.B1;
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int C = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < C; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int[] scores = new int[N];
+            int sum = 0;
+
+            for (int j = 0; j < N; j++) {
+                scores[j] = Integer.parseInt(st.nextToken());
+                sum += scores[j];
+            }
+
+            double avg = (double) sum / N;
+            int count = 0;
+            for (int score : scores) {
+                if (score > avg) {
+                    count++;
+                }
+            }
+            double percent = (double) count / N * 100;
+            System.out.printf("%.3f%%\n", percent);
+        }
+    }
+}
