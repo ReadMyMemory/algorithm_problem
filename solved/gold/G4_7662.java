ed5da41d9d0cd09541a87628501958978b2d9645
ReadMyMemory <lame3925@gmail.com>
2025-09-07 01:03:39 +0900

    Solved G4_7662

---
+++ solved/gold/G4_7662.java
@@ -0,0 +1,57 @@
+package solved.gold;
+import java.io.*;
+import java.util.*;
+
+// 이중 우선순위 큐를 사용하는데 있어서, 2개의 PriorityQueue를 사용하지 말고, TreeMap을 사용.,
+
+public class G4_7662 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        while (T-- > 0) {
+            int k = Integer.parseInt(br.readLine());
+            TreeMap<Integer, Integer> map = new TreeMap<>();
+
+            for (int i = 0; i < k; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                String op = st.nextToken();
+                int num = Integer.parseInt(st.nextToken());
+
+                if (op.equals("I")) {
+                    // 삽입 연산
+                    map.put(num, map.getOrDefault(num, 0) + 1);
+                } else {
+                    // 삭제 연산
+                    if (map.isEmpty()) continue;
+
+                    if (num == 1) {
+                        // 최댓값 삭제
+                        int maxKey = map.lastKey();
+                        if (map.get(maxKey) == 1) {
+                            map.remove(maxKey);
+                        } else {
+                            map.put(maxKey, map.get(maxKey) - 1);
+                        }
+                    } else {
+                        // 최솟값 삭제
+                        int minKey = map.firstKey();
+                        if (map.get(minKey) == 1) {
+                            map.remove(minKey);
+                        } else {
+                            map.put(minKey, map.get(minKey) - 1);
+                        }
+                    }
+                }
+            }
+            if (map.isEmpty()) {
+                sb.append("EMPTY\n");
+            } else {
+                sb.append(map.lastKey()).append(" ").append(map.firstKey()).append("\n");
+            }
+        }
+        System.out.print(sb);
+    }
+}
