54b2e3154ef26a5e73ed180aae6fa7a1be231c67
ReadMyMemory <lame3925@gmail.com>
2026-02-09 23:18:40 +0900

    Solved B2_15596

---
+++ solved/bronze/B2/B2_15596.java
@@ -0,0 +1,11 @@
+package solved.bronze.B2;
+
+public class B2_15596 {
+    long sum(int[] a) {
+        long total = 0;
+        for (int x : a) {
+            total += x;
+        }
+        return total;
+    }
+}
