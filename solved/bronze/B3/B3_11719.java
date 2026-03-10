ef704a190b6c1fe555ae51fd456b118d036658ec
ReadMyMemory <lame3925@gmail.com>
2026-03-10 23:28:46 +0900

    Solved B3_11719

---
+++ solved/bronze/B3/B3_11719.java
@@ -0,0 +1,16 @@
+package solved.bronze.B3;
+import java.io.*;
+
+public class B3_11719 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String line;
+        while ((line = br.readLine()) != null) {
+            sb.append(line).append('\n');
+        }
+
+        System.out.print(sb);
+    }
+}
