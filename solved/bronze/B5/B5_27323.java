0d56b627387df2ea887609283da381844680df84
ReadMyMemory <lame3925@gmail.com>
2026-03-30 21:48:10 +0900

    Solved B5_27323

---
+++ solved/bronze/B5/B5_27323.java
@@ -0,0 +1,17 @@
+package solved.bronze.B5;
+import java.io.*;
+
+public class B5_27323 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+
+        bw.write(String.valueOf(A * B));
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
