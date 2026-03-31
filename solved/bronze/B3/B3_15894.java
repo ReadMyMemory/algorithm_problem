29060f0ed8cfe634c868f321647fd8600d0d38ac
ReadMyMemory <lame3925@gmail.com>
2026-04-01 02:26:39 +0900

    Solved B3_15894

---
+++ solved/bronze/B3/B3_15894.java
@@ -0,0 +1,10 @@
+package solved.bronze.B3;
+import java.io.*;
+
+public class B3_15894 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long n = Long.parseLong(br.readLine());
+        System.out.println(n * 4);
+    }
+}
